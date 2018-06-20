217aecebe982518f5c341e6776b063b18cc8081a
Olavi Haapala <ojhaapala@gmail.com>
2018-06-20 22:59:25 +0300

    Display reasons in the UI if broken

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import H1 from '../elements/H1';
 import H2 from '../elements/H2';
 import P from '../elements/P';
 import A from '../elements/A';
+import Span from '../elements/Span';
 import theme from '../theme';
 import { css } from 'react-emotion';
 
@@ -11,7 +12,7 @@ export default class Index extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      loading: true,
+      loading: true
     };
   }
 
@@ -39,9 +40,15 @@ export default class Index extends React.Component {
             <P lineHeight="copy">Ladataan statusta...</P>
           ) : this.state.status ? (
             this.state.status.broken ? (
-              <H2 fontSize={[3, 4, 6]} color="red.6" lineHeight="copy">
-                Kyllä!
-              </H2>
+              <Div>
+                <H2 fontSize={[3, 4, 6]} color="red.6" lineHeight="copy">
+                  Kyllä!
+                </H2>
+                {this.state.status.reasons &&
+                  this.state.status.reasons.map(reason => (
+                    <Span>{reason}</Span>
+                  ))}
+              </Div>
             ) : (
               <H1 fontSize={[3, 4, 6]} color="green.8" lineHeight="copy">
                 Ei!
