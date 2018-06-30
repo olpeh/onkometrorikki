b4970c02f5304af00570a863c6e01790dde1301b
Olavi Haapala <ojhaapala@gmail.com>
2018-06-30 16:40:12 +0300

    Clear the decks – rewrite the UI in elm

--- elements/Em.js
+++
@@ -1,9 +0,0 @@
-import styled from 'react-emotion';
-import { space, fontSize, fontWeight, color } from 'styled-system';
-import tag from 'clean-tag';
-
-const Em = styled(tag.em)(space, fontSize, fontWeight, color, {});
-
-Em.defaultProps = {};
-
-export default Em;
--- elements/U.js
+++
@@ -1,9 +0,0 @@
-import styled from 'react-emotion';
-import { space, width, fontSize, fontWeight, color } from 'styled-system';
-import tag from 'clean-tag';
-
-const U = styled(tag.u)(space, width, fontSize, fontWeight, color, {});
-
-U.defaultProps = {};
-
-export default U;
