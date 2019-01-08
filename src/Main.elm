9942b54ac5cfd848c5731cb31fef1a6b40716269
fpapado <fotis@futurice.com>
2019-01-08 20:30:19 +0200

    NoOp on onClick when refreshing, vs. disabling

--- src/Main.elm
+++ src/Main.elm
@@ -56,6 +56,7 @@ type Msg
     | Refresh Time.Posix
     | UpdateTime Time.Posix
     | AdjustTimeZone Time.Zone
+    | NoOp
 
 
 
@@ -83,6 +84,9 @@ update msg model =
             ( { model | zone = newZone }
             , Task.perform Refresh Time.now
             )
+
+        NoOp ->
+            ( model, Cmd.none )
 
 
 
@@ -204,8 +208,7 @@ viewRefreshButton statusRequest theme =
                     False
     in
     button
-        [ disabled isRefreshing
-        , class "button-reset refresh-button enhanced-outline"
+        [ class "button-reset refresh-button enhanced-outline"
         , class
             (if isRefreshing then
                 "refreshing"
@@ -213,10 +216,17 @@ viewRefreshButton statusRequest theme =
              else
                 ""
             )
-
-        -- Show this button also with inverted colors
         , class (Theme.toClass (Theme.invert theme))
-        , onClick (Refresh (Time.millisToPosix 0))
+
+        -- Instead of disabling the button, remove the onClick handler when submitting
+        , onClick
+            (case isRefreshing of
+                True ->
+                    NoOp
+
+                False ->
+                    Refresh (Time.millisToPosix 0)
+            )
         ]
         [ span [ class "refresh-button-label" ] [ text "Refresh" ]
         , FeatherIcons.refreshCw
