e925510b3169c8e6f38444f41e6bec2ec4862ccd
ohanhi <ossi.hanhinen@gmail.com>
2019-01-18 21:41:15 +0200

    Add viewStatus test and Travis config

---
+++ tests/ViewStatusTest.elm
@@ -0,0 +1,36 @@
+module ViewStatusTest exposing (statusFuzzer, suite)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Main
+import Status exposing (Status(..))
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector as Selector
+import Time
+
+
+suite : Test
+suite =
+    fuzz statusFuzzer "viewStatus should show correct option" <|
+        \status ->
+            Main.viewStatus status (Time.millisToPosix 0) Time.utc
+                |> Query.fromHtml
+                |> Query.find [ Selector.tag "h2" ]
+                |> Query.has
+                    (case status of
+                        Working ->
+                            [ Selector.text "Ei" ]
+
+                        Broken reasons ->
+                            [ Selector.text "Kyllä" ]
+                    )
+
+
+statusFuzzer : Fuzzer Status
+statusFuzzer =
+    Fuzz.oneOf
+        [ Fuzz.list Fuzz.string
+            |> Fuzz.map Broken
+        , Fuzz.constant Working
+        ]
