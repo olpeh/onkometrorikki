025306bb8a2b15cffdbd264fe1a3d2f2fc207ec7
Olavi Haapala <olavi.haapala@futurice.com>
2019-06-04 23:14:40 +0300

    Add endpoint for getting the result

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ const bodyParser = require('koa-bodyparser');
 const route = require('koa-route');
 const cacheControl = require('koa-cache-control');
 import { createResponse, fetchFeed } from './hsl';
+import { rejects } from 'assert';
 
 const respondFromHSL = async (
   ctx,
@@ -140,14 +141,53 @@ export const setUpApp = (redisClient, port, cacheTtlSeconds, cacheKey) => {
 
   app.use(
     route.post('/slack/actions', async ctx => {
-      const payload = ctx.request.body;
-      if (payload.type === 'url_verification') {
-        ctx.response.statusCode = 200;
-        // TODO: verify this
-        // Once you receive the event, verify the request's authenticity and
-        // then respond in plaintext with the challenge attribute value
-        ctx.response.body = payload.challenge;
-      }
+      await new Promise((resolve, reject) => {
+        const payload = ctx.request.body;
+
+        if (payload.type === 'url_verification') {
+          ctx.response.statusCode = 200;
+          // TODO: verify this
+          // Once you receive the event, verify the request's authenticity and
+          // then respond in plaintext with the challenge attribute value
+          ctx.response.body = payload.challenge;
+          resolve();
+        }
+
+        if (payload.type === 'app_mention') {
+          if (redisClient) {
+            redisClient.get(cacheKey, async (error, result) => {
+              let answer = '42';
+              if (result) {
+                console.log('Slack bot is serving the response from cache', {
+                  error,
+                  result
+                });
+                const status = JSON.parse(result);
+                const question = 'Onko metro rikki? – ';
+                if (status.broken) {
+                  // TODO: add I18N and reuse implementation between bots
+                  const finnishReason = status.reasons[0][0].text;
+                  answer = `${question} Kyllä!\n${finnishReason}`;
+                } else {
+                  answer = `${question} Ei!`;
+                }
+              } else {
+                console.warn(
+                  'Status was not found in cache, Telegram bot failed to serve status'
+                );
+                answer =
+                  'Jokin meni pieleen... Kokeile myöhemmin uudestaan tai vieraile https://onkometrorikki.fi sivulla.';
+              }
+              ctx.response.statusCode = 200;
+              ctx.response.body = answer;
+              resolve();
+            });
+          }
+        } else {
+          console.warn('Redis not available, ignoring app_mention');
+          reject();
+        }
+      });
     })
   );
 
