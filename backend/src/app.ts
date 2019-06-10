3a1fbec4eb0ce4613de5220d305e10f2ddd4ad5d
Olavi Haapala <olavi.haapala@futurice.com>
2019-06-10 19:22:40 +0300

    Avoid responding multiple times to same event and avoid timeouts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,10 @@ import { createResponse, fetchFeed } from './hsl';
 import { postSlackMessage } from './slack';
 const slackToken = process.env.SLACK_BOT_TOKEN;
 
+const seenEventIds: string[] = [];
+
+const eventSeenBefore = (id: string) => seenEventIds.includes(id);
+
 const respondFromHSL = async (
   ctx,
   resolve,
@@ -142,84 +146,92 @@ export const setUpApp = (redisClient, port, cacheTtlSeconds, cacheKey) => {
 
   app.use(
     route.post('/slack/actions', async ctx => {
-      await new Promise((resolve, reject) => {
-        const payload = ctx.request.body;
+      const payload = ctx.request.body;
+      // Assume success, set to error in case of error
+      ctx.response.statusCode = 200;
 
-        if (payload.type === 'url_verification') {
-          ctx.response.statusCode = 200;
-          // TODO: verify this
-          // Once you receive the event, verify the request's authenticity and
-          // then respond in plaintext with the challenge attribute value
-          ctx.response.body = payload.challenge;
-          resolve();
-        } else if (
-          (payload.event.type === 'app_mention' ||
-            payload.event.type === 'message') &&
-          (typeof payload.event.subtype === 'undefined' ||
-            // Don't react to messages sent by a bot (this bot for example!)
-            (payload.event.subtype && payload.event.subtype !== 'bot_message'))
-        ) {
-          /*
+      if (payload.type === 'url_verification') {
+        // TODO: verify this
+        // Once you receive the event, verify the request's authenticity and
+        // then respond in plaintext with the challenge attribute value
+        ctx.response.body = payload.challenge;
+        return;
+      } else if (
+        (payload.event.type === 'app_mention' ||
+          payload.event.type === 'message') &&
+        (typeof payload.event.subtype === 'undefined' ||
+          // Don't react to messages sent by a bot (this bot for example!)
+          (payload.event.subtype && payload.event.subtype !== 'bot_message'))
+      ) {
+        if (eventSeenBefore(payload.event.client_msg_id)) {
+          console.log(
+            `Event with id: ${
+              payload.event.client_msg_id
+            } has been seen before, ignoring....`
+          );
+          return;
+        }
+
+        seenEventIds.push(payload.event.client_msg_id);
+        /*
           See https://api.slack.com/events/app_mention
               https://api.slack.com/events/message.im
           */
-          console.log('Slack bot received', payload);
+        console.log('Slack bot received', payload);
 
-          if (!slackToken) {
-            console.warn('Slack token missing! Ignoring slack event received');
-            reject();
-          }
+        if (!slackToken) {
+          const errorMessage =
+            'Slack token missing! Ignoring slack event received';
+          console.warn(errorMessage);
+          ctx.response.statusCode = 500;
+          ctx.response.body = errorMessage;
+          return;
+        }
 
-          if (redisClient) {
-            redisClient.get(cacheKey, async (error, result) => {
-              let answer = '42';
-              if (result) {
-                console.log('Slack bot is serving the response from cache', {
-                  error,
-                  result
-                });
-                const status = JSON.parse(result);
-                const question = 'Onko metro rikki? – ';
-                if (status.broken) {
-                  // TODO: add I18N and reuse implementation between bots
-                  const finnishReason = status.reasons[0][0].text;
-                  answer = `${question} Kyllä!\n${finnishReason}`;
-                } else {
-                  answer = `${question} Ei!`;
-                }
-              } else {
-                console.warn(
-                  'Status was not found in cache, Telegram bot failed to serve status'
-                );
-                answer =
-                  'Jokin meni pieleen... Kokeile myöhemmin uudestaan tai vieraile https://onkometrorikki.fi sivulla.';
-              }
-              ctx.response.statusCode = 200;
-              ctx.response.body = 'OK';
-              postSlackMessage({
-                channel: payload.event.channel,
-                text: answer,
-                token: slackToken
+        if (redisClient) {
+          redisClient.get(cacheKey, async (error, result) => {
+            let answer = '42';
+            if (result) {
+              console.log('Slack bot is serving the response from cache', {
+                error,
+                result
               });
-              resolve();
-            });
-          } else {
-            const msg =
-              'Redis not available, ignoring app_mention due to performance';
-            console.warn(msg);
-            ctx.response.statusCode = 200;
-            ctx.response.body = 'OK';
+              const status = JSON.parse(result);
+              const question = 'Onko metro rikki? – ';
+              if (status.broken) {
+                // TODO: add I18N and reuse implementation between bots
+                const finnishReason = status.reasons[0][0].text;
+                answer = `${question} Kyllä!\n${finnishReason}`;
+              } else {
+                answer = `${question} Ei!`;
+              }
+            } else {
+              console.warn(
+                'Status was not found in cache, Telegram bot failed to serve status'
+              );
+              answer =
+                'Jokin meni pieleen... Kokeile myöhemmin uudestaan tai vieraile https://onkometrorikki.fi sivulla.';
+            }
+
             postSlackMessage({
               channel: payload.event.channel,
-              text: msg,
+              text: answer,
               token: slackToken
             });
-            resolve();
-          }
+          });
         } else {
-          console.warn('Unknown payload, ignoring');
+          const msg =
+            'Redis not available, ignoring app_mention due to performance';
+          console.warn(msg);
+          postSlackMessage({
+            channel: payload.event.channel,
+            text: msg,
+            token: slackToken
+          });
         }
-      });
+      } else {
+        console.warn('Unknown payload, ignoring');
+      }
     })
   );
 
