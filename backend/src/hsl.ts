f65028eb9745973082f2848dd116eb7a2d34c497
Olavi Haapala <olavi.haapala@futurice.com>
2019-08-25 10:45:53 +0300

    Switch from gtfs API to GraphQL API
    Fixes and closes #31

--- backend/src/hsl.ts
+++ backend/src/hsl.ts
@@ -1,73 +1,52 @@
-const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const request = require('request');
 import { Status } from './../models/Status';
 
 require('dotenv').config();
 const debugBrokenRandomly = process.env.DEBUG_BROKEN_RANDOMLY || false;
 
-/*
-Deprecated Types
-The values below are retained for backwards-compatibility with existing feeds;
-feed-reading applications should continue to understand these, but they shouldn't be used in new feeds.
-s://sites.google.com/site/gtfschanges/proposals/route-type
-
-Value
-	Name
-	Corresponding New Value
-0
-	Tram, Light Rail, Streetcar
-	900
-1
-	Subway, Metro
-	400
-2
-	Rail
-	100
-3
-	Bus
-	700
-4
-	Ferry 	1000
-5
-	Cable Car
-	1701
-6
-	Gondola, Suspended cable car
-	1300
-7
-	Funicular
-	1400
-*/
-const METRO_ROUTE_TYPE_OLD = 1;
-const METRO_ROUTE_TYPE_NEW = 400;
-
 export const fetchFeed = async () => {
   console.log('Going to fetch from the external API');
   return new Promise((resolve, reject) => {
-    if (debugBrokenRandomly && Math.random() >= 0.5) {
+    if (debugBrokenRandomly === 'true' && Math.random() >= 0.5) {
       console.log(`******************** Debug RANDOMLY BREAK mode on!*************************
       Serving alerts randomly from test binary feed and randomly from real feed!`);
       const fs = require('fs');
       const path = require('path');
-      var content = fs.readFileSync(
-        path.join(__dirname, '../tests/assets/alerts')
+      const content = fs.readFileSync(
+        path.join(__dirname, '../tests/assets/graphql-response.json')
       );
-      const feed = GtfsRealtimeBindings.FeedMessage.decode(content);
+      const feed = JSON.parse(content).data.alerts;
       resolve(feed);
     } else {
       const requestOptions = {
-        method: 'GET',
-        url: 'https://api.digitransit.fi/realtime/service-alerts/v1/',
-        encoding: null
+        method: 'POST',
+        url: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+        headers: { 'Content-Type': 'application/graphql' },
+        body: `{
+          alerts(severityLevel: [WARNING, UNKNOWN_SEVERITY, SEVERE]) {
+            alertDescriptionTextTranslations {
+              text
+              language
+            }
+            route{
+              mode
+            }
+          }
+        }`
       };
 
       request(requestOptions, function(error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log('Fetch successful!');
-          const feed = GtfsRealtimeBindings.FeedMessage.decode(body);
-          resolve(feed);
+          try {
+            const alerts = JSON.parse(body).data.alerts;
+            resolve(alerts);
+          } catch (e) {
+            console.error(e);
+            reject(e);
+          }
         } else {
-          console.error(error);
+          console.error(error, JSON.stringify(JSON.parse(body), null, 4));
           reject(error);
         }
       });
@@ -88,36 +67,16 @@ export const createResponse = (feed): Status => {
       reasons: ['Failed to fetch the feed. The Metro might work or might not.']
     };
   } else {
-    let brokenCount = 0;
-    let reasons = [];
-    feed.entity.forEach(function(entity) {
-      let thisAlertForBrokenMetro = false;
-      if (entity.alert) {
-        if (
-          entity.alert &&
-          entity.alert.informed_entity &&
-          entity.alert.informed_entity.length > 0 &&
-          (entity.alert.informed_entity[0].route_type == METRO_ROUTE_TYPE_OLD ||
-            entity.alert.informed_entity[0].route_type == METRO_ROUTE_TYPE_NEW)
-        ) {
-          brokenCount++;
-          thisAlertForBrokenMetro = true;
-        }
-
-        if (
-          thisAlertForBrokenMetro &&
-          entity.alert.description_text &&
-          entity.alert.description_text.translation &&
-          entity.alert.description_text.translation.length > 0
-        ) {
-          reasons.push(entity.alert.description_text.translation);
-        }
-      }
-    });
+    const reasons = feed
+      // TODO: Why can't GraphQL do this filtering for me already?
+      .filter(
+        alert => alert.route && alert.route && alert.route.mode === 'SUBWAY'
+      )
+      .map(alert => alert.alertDescriptionTextTranslations);
 
     return {
       ...defaultResponse,
-      broken: brokenCount > 0,
+      broken: reasons && reasons.length > 0,
       reasons: [...defaultResponse.reasons, ...reasons]
     };
   }
